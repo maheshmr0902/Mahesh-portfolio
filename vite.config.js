591766232c04ffb1d5cdf758dbad73f9db64031f
maheshmr0902 <148605303+maheshmr0902@users.noreply.github.com>
2025-06-24 19:44:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Replace 'your-repo-name' with the actual name of your GitHub repo
 export default defineConfig({
+  base: './', // ✅ Fixes blank screen on Netlify
   plugins: [react()],
-  base: '/Mahesh-portfolio/', // 👈 VERY IMPORTANT
 });
+
 
 
